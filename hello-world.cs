995f0f39f3d342301935e0330197acfebc76014e
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-02-14 10:32:58 +0200

    Adding new files to repository.

---
+++ hello-world.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Ekoodi
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Testing commenting. Testing print text next:
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            
+        }
+    }
+}
